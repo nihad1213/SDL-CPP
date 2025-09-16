b959f1cadaef0eb61394dd410629b4b2a710dd90
nihad1213 <nihad.nemetli@gmail.com>
2025-09-17 01:45:35 +0400

    test sdl files (Makefile, main.cpp) files created

---
+++ test-sdl/main.cpp
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main() {
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+    if (!win) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    
+
+    return 0;
+}
