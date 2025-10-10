52124c26213a72a71ac6c59946bcd7181c3c70e4
nihad1213 <nihad.nemetli@gmail.com>
2025-10-11 00:30:42 +0400

    new: main.cpp file created. Game.hpp included (doesnt work due to uncomplete functions)

---
+++ improved-character-movement/main.cpp
@@ -0,0 +1,16 @@
+#include "Game/Game.hpp"
+#include <iostream>
+
+int main() {
+    Game game;
+
+    if (!game.Init()) {
+        std::cerr << "Failed to initialize the game" << std::endl;
+        return -1;
+    }
+
+    game.Run();
+    game.Cleanup();
+
+    return 0;
+}
